c341554149c431610a6c5b2386a2ab638178e0f9
andymehi <andymehi@hotmail.com>
2019-05-14 11:20:40 -0400

    complete name test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
+  fixtures :users
+
+  def setup
+    @user = users(:one)
   end
+
+  test "Complete name" do
+    assert_equal "juan pablo ", @user.complete_name
+  end
+
 end
