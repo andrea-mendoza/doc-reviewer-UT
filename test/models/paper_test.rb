7655fd3a05601db17cf5e53b9731aa772f2f5db2
Julia1297 <valita129@gmail.com>
2019-05-16 00:03:27 -0400

    Tests

--- test/models/paper_test.rb
+++ test/models/paper_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class PaperTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :papers
+
+  def setup
+    @paper = papers(:one)
+  end
+
+  test "belongs an user" do
+    assert_equal @paper.user , "juan.pablo@sandoval.com "
+  end
+
+  test "has a title" do
+    assert_equal @paper.title , "Mi documento"
+  end
+
+  test "belongs an event" do
+    assert_equal @paper.event , "one"
+  end
+
+  test "has a resume" do
+    assert_equal @paper.user , "Este es mi primer documento"
+  end
+  
 end
