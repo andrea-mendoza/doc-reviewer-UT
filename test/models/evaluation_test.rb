40c449ee380de73003ba440441b2c8b0760f77da
andymehi <andymehi@hotmail.com>
2019-05-15 23:52:44 -0400

    evaluation tests

--- test/models/evaluation_test.rb
+++ test/models/evaluation_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class EvaluationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @evaluation = evaluations(:one)
+  end
+
+  test 'evaluation is quantitative' do
+    assert_equal 1.5, @evaluation.is_quantitative?
+  end
+
+  test 'evaluation is qualitative' do
+    assert_equal "MyText", @evaluation.is_qualitative?
+  end
 end
