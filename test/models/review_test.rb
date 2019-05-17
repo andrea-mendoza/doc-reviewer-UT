602f8e23edc21406a0eb003579a3bd275b862363
Julia1297 <valita129@gmail.com>
2019-05-17 10:59:33 -0400

    Review has relevance

--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class ReviewTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @review = reviews(:one)
+  end
+
+  test "has relevance " do
+    assert_equal @review.relevance , "High"
+  end
+  # relevance: High
+  # problem_def: Problem 1
+  # general_def: General 1
+  # specific_def: Specific 1
+  # confidence: Confidence 1
+  # general_evaluation: Good
+  # personal_message: Good job
+  # paper: one
 end
