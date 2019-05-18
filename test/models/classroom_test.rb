d79d10037aaf8a1b4174be63fd8f687be0b087bf
Maye Germanotta <30674218+maye-gallardo@users.noreply.github.com>
2019-05-17 22:01:35 -0400

    Revert "classroom tests"

--- test/models/classroom_test.rb
+++ test/models/classroom_test.rb
@@ -1,27 +1,7 @@
 require 'test_helper'
 
 class ClassroomTest < ActiveSupport::TestCase
-  fixtures :classrooms
-
-  def setup
-    @classroom = classrooms(:one)
-    @paper = papers(:one)
-  end
-
-  test "enter an ambient" do
-    assert_equal @classroom.ambient , "AP-2"
-  end
-
-  test "choose a date" do
-    assert_equal @classroom.defense_date , "26 Abr, 2019"
-  end
-
-  test "choose a hour" do
-    assert_equal @classroom.defense_hour , "03:30 PM"
-  end
-
-  test "belongs an paper" do
-    assert_equal @paper.paper , @paper
-  end
-
+  # test "the truth" do
+  #   assert true
+  # end
 end
