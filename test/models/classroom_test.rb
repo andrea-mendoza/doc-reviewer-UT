5151f866088c053506721df9efecd2a6ae07c366
Maria de los Angeles Gallardo <maye.gallardoger@gmail.com>
2019-05-17 21:45:51 -0400

    classroom tests

--- test/models/classroom_test.rb
+++ test/models/classroom_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class ClassroomTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :classrooms
+
+  def setup
+    @classroom = classrooms(:one)
+    @paper = papers(:one)
+  end
+
+  test "enter an ambient" do
+    assert_equal @classroom.ambient , "AP-2"
+  end
+
+  test "choose a date" do
+    assert_equal @classroom.defense_date , "26 Abr, 2019"
+  end
+
+  test "choose a hour" do
+    assert_equal @classroom.defense_hour , "03:30 PM"
+  end
+
+  test "belongs an paper" do
+    assert_equal @paper.paper , @paper
+  end
+
 end
