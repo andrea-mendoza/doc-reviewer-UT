d2d001b1f1d5d3c84b3cd40af990f291c7200342
Maria de los Angeles Gallardo <maye.gallardoger@gmail.com>
2019-05-17 22:27:38 -0400

    classroom tests

--- test/models/classroom_test.rb
+++ test/models/classroom_test.rb
@@ -8,20 +8,20 @@ class ClassroomTest < ActiveSupport::TestCase
     @paper = papers(:one)
   end
 
+  test "belongs an paper" do
+    assert_equal @paper.paper , @paper
+  end
+
   test "enter an ambient" do
     assert_equal @classroom.ambient , "MyString"
   end
 
   test "choose a date" do
-    assert_equal @classroom.defense_date , "Sat, 03 Nov 2018"
+    assert_equal @classroom.defense_date , " Sat, 03 Nov 2018"
   end
 
   test "choose a hour" do
     assert_equal @classroom.defense_hour , "Sat, 01 Jan 2000 08:46:06 -04 -04:00"
-  end
-
-  test "belongs an paper" do
-    assert_equal @paper.paper , @paper
   end
 
 end
