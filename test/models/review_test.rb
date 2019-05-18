3602874459932d8daecda3dceb954fdbe3fafeec
Julia1297 <valita129@gmail.com>
2019-05-17 22:48:04 -0400

    Final score initial

--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -7,10 +7,15 @@ class ReviewTest < ActiveSupport::TestCase
   def setup
     @review = reviews(:one)
     @user = users(:one)
+    @question = questions(:one)
   end
 
   test "has a  user " do
     assert_equal @user, @review.user
+  end
+
+  test "final score initial should be 0 " do
+    assert_equal 0, @review.final_score
   end
 
 
