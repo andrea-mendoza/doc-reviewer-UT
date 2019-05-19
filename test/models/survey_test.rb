6583c7c07aa1957c20bbec44ee91a3beabbaec6f
Maria de los Angeles Gallardo <maye.gallardoger@gmail.com>
2019-05-19 16:55:33 -0400

    survery tests

--- test/models/survey_test.rb
+++ test/models/survey_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class SurveyTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :surveys
+
+  def setup
+    @survey = surveys(:one) 
+end
+
+test "enter an name" do
+  assert_equal @survey.name , "MyName"
+end
+
+test "enter an questions_attributes" do
+  assert_equal @survey.questions_attributes , {1,"my document","trabajo practico","monografia"}
+end
+
 end
