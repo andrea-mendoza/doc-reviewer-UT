cc43081337b9b3b5fadcd0c0cf600ac7bbe16cbb
andymehi <andymehi@hotmail.com>
2019-05-18 23:34:38 -0400

    option tests

--- test/models/option_test.rb
+++ test/models/option_test.rb
@@ -7,7 +7,31 @@ class OptionTest < ActiveSupport::TestCase
         @option = options(:one) 
     end
 
+    test 'valid option' do
+        assert @option.valid?
+    end
+    
+    test 'invalid option without point and description' do
+        auxOption = Option.new
+        assert_not auxOption.valid?
+    end
+
+    test 'invalid option without point ' do
+        auxOption = Option.new(description:"Excellent")
+        assert_not auxOption.valid?
+    end
+
+    test 'invalid option without description' do
+        auxOption = Option.new(point:7)
+        assert_not auxOption.valid?
+    end
+
+    test "Show option value" do 
+        assert_equal 3, @option.value
+    end  
+
     test "Show a point and a description" do 
         assert_equal "(3) Good", @option.show
     end 
+    
 end
