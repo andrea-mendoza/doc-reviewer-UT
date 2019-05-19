18ebae55797697b884031e9fd20217a4b7bb9d4a
Maria de los Angeles Gallardo <maye.gallardoger@gmail.com>
2019-05-19 18:12:55 -0400

    survey tests

--- test/models/survey_test.rb
+++ test/models/survey_test.rb
@@ -11,6 +11,8 @@ test "enter an name" do
   assert_equal @survey.name , "FirstProfileRevision"
 end
 
-
-
+test "select quantitative surveys  " do
+  assert_equal @survey.quantitative_questions(), [@survey]
 end
+
+
