c2572d3cc1d0faed07322213fe31bc2217a0cdcf
andymehi <andymehi@hotmail.com>
2019-05-19 01:41:05 -0400

    ability tests

--- test/models/ability_test.rb
+++ test/models/ability_test.rb
@@ -4,13 +4,44 @@ class AbilityTest < ActiveSupport::TestCase
     fixtures :users
 
     def setup
-        @user = users(:one)
+        @student = users(:one)
+        @director = users(:two)
         @review = reviews(:one)
-        @ability = Ability.new(@user)
+        @event = events(:one) 
+        @paper = papers(:one)
+        @classroom = classrooms(:one)
     end
     
-    test "user with student role can manage a review" do
-        assert @user.valid?
-        assert @ability.can?(:manage, @review)
+    test "student role can manage a review" do
+        ability = Ability.new(@student)
+        assert @student.valid?
+        assert ability.can?(:manage, @review)
+    end
+
+    test "student role can manage a paper" do
+        @student.role = "estudiante"
+        ability = Ability.new(@student)
+        assert @student.valid?
+        assert ability.can?(:manage, @paper)
+    end
+
+    test "director can manage a student" do
+        @director.role = "director"
+        ability = Ability.new(@director)
+        assert ability.can?(:manage, @student)
+    end
+
+    test "users can read Events, Papers and Reviews" do
+        @student.role = "estudiante"
+        ability = Ability.new(@student)
+        assert ability.can?(:read, @event, @paper, @review)
+    end
+
+    test "admin can manage Event, Paper, Classroom" do
+        @director.role = "director"
+        @director.admin_role = true
+        ability = Ability.new(@director)
+        assert @director.valid?
+        assert ability.can?(:manage, @event,@paper,@classroom)
     end
 end
