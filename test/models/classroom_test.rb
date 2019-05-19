69f1b755247da9134c57b85b88746873384a704d
Maria de los Angeles Gallardo <maye.gallardoger@gmail.com>
2019-05-18 22:52:51 -0400

    return test

--- test/models/classroom_test.rb
+++ test/models/classroom_test.rb
@@ -11,13 +11,8 @@ class ClassroomTest < ActiveSupport::TestCase
     assert_equal @classroom.ambient , "MyString"
   end
 
-  test "choose a date" do
-    assert_equal "Sat, 03 Nov 2018",@classroom.defense_date
-  end
-
   test "choose a hour" do
     assert_equal @classroom.defense_hour , "Sat, 01 Jan 2000 08:46:06 -04 -04:00"
   end
-
 
 end
