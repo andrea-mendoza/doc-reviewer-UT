3992d0c0255c9df8931f261b092814983d68f444
Julia1297 <valita129@gmail.com>
2019-05-20 22:10:12 -0400

    Correccion questions.yml

--- test/models/question_test.rb
+++ test/models/question_test.rb
@@ -5,6 +5,8 @@ class QuestionTest < ActiveSupport::TestCase
 
   def setup
     @question = questions(:one)
+    @question1 = questions(:two)
+
   end
 
   test "has a  description " do
@@ -15,12 +17,20 @@ class QuestionTest < ActiveSupport::TestCase
     assert_equal 2, @question.max_score
   end
 
-  test "display type " do
-    assert_equal "Type not set", @question.display_type
+  test "display type  not selected" do
+    assert_equal "Type not set", @question1.display_type
+  end
+
+  test "display type  selected text" do
+    assert_equal "Texto", @question.display_type
   end
 
   test "is quantitative " do
-    assert_equal 2, @question.is_quantitative? 
+    assert_equal 2, @question1.is_quantitative? 
+  end
+
+  test "is quantitative " do
+    assert_equal 2, false 
   end
 
 
